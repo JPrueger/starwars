69ecbf9e5a9d84a38926e8fae23e82eedb18fcb3
Jeannine Prueger <jeannine.prueger@gmail.com>
2020-11-10 13:52:45 +0100

    added styling to cards (shown details of each character)

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -1,17 +1,20 @@
 <template>
 
   <div>
-
-     <div class="c" v-for="character in list" v-bind:key="character.name">
-        <h2>Name: {{ character.name }}</h2>
-        <p>{{ character.height }}</p>
-        <p>{{ character.mass }}</p>
-        <p>{{ character.hair_color }}</p>
-        <p>{{ character.skin_color }}</p>
-        <p>{{ character.birth_year }}</p>
-        <p>{{ character.gender }}</p>
+    <h2>Here's a list of all Star Wars characters:</h2>
+    <div class="wrapper_card">
+      <div class="c" v-for="character in list" v-bind:key="character.name">
+        <div class="card">
+          <h3>{{ character.name }}</h3>
+          <p>Height: {{ character.height }}</p>
+          <p>Mass: {{ character.mass }}</p>
+          <p>Hair Color: {{ character.hair_color }}</p>
+          <p>Skin Color: {{ character.skin_color }}</p>
+          <p>Birth Year: {{ character.birth_year }}</p>
+          <p>Gender: {{ character.gender }}</p>
+        </div>
       </div>
-
+    </div>
   </div>
 
 </template>
@@ -46,7 +49,25 @@ export default {
 
 <style lang="scss">
 
- @import '@/assets/styles/variables.scss';
+  @import '@/assets/styles/variables.scss';
+
+  .wrapper_card {
+    display: grid;
+    grid-column-gap: 35px;
+    padding-left: 10rem;
+    padding-right: 10rem;
+    grid-auto-flow: row;  
+    grid-template-columns: repeat(auto-fit,minmax(300px, 1fr)); 
+    margin-bottom: 2rem;
+  }
+
+  .card {
+    background-color: white;
+    margin-bottom: 3rem;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24); 
+    border-radius: 5px;
+    padding: 20px 0 20px 0;
+  }
 
 
 </style>
