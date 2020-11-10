30ee0577591c0ac0855c2049111d6ce5ff8e7640
Jeannine Prueger <jeannine.prueger@gmail.com>
2020-11-10 13:32:26 +0100

    updated characters component (axios - get swapi), v-for through list of characters and shown details

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -1,20 +1,52 @@
 <template>
 
   <div>
-    <h1>Let's talk about Star Wars!</h1>
 
-    <p>Get all the information you need about every character.</p>
+     <div class="c" v-for="character in list" v-bind:key="character.name">
+        <h2>Name: {{ character.name }}</h2>
+        <p>{{ character.height }}</p>
+        <p>{{ character.mass }}</p>
+        <p>{{ character.hair_color }}</p>
+        <p>{{ character.skin_color }}</p>
+        <p>{{ character.birth_year }}</p>
+        <p>{{ character.gender }}</p>
+      </div>
+
   </div>
 
 </template>
+
+<script>
+
+import Vue from 'vue'
+import Axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, Axios)
+
+export default {
+    name: "Characters",
+
+    data () {
+        return {list:undefined}
+    },
+
+    mounted () {
+        Vue.axios.get('https://swapi.dev/api/people/')
+        .then((response) => {
+            this.list=response.data.results
+            // console.warn(response.results)
+            console.log(response.data.results)
+        })
+    }
+}
+
+</script>
 
 
 <style lang="scss">
 
  @import '@/assets/styles/variables.scss';
 
- h1 {
-   color: $primaryColor;
- }
 
 </style>
